bebc7a39c4c81c122571db0c4fb76d888747803b
Michael Roger <bodgeuk12@gmail.com>
2020-01-17 10:28:56 +0000

    Added font-awesome icons.

---
+++ src/components/Icons.js
@@ -0,0 +1,5 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTv } from '@fortawesome/free-solid-svg-icons'
+
+export const TvIcon = (props) => <FontAwesomeIcon icon={faTv} {...props} />
