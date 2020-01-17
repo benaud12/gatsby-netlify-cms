83f3fdbce5f481b56127cf46863b4a143948e18c
Michael Roger <bodgeuk12@gmail.com>
2020-01-17 10:29:41 +0000

    Fix google maps iframe props names.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,8 @@ export default () => (
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1349.6494224834978!2d13.628670900568657!3d47.425615859635144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47713029f1ff093f%3A0xa00c4e4cf1b65e8e!2sHaus%20Karlwirt!5e0!3m2!1sen!2suk!4v1569166214716!5m2!1sen!2suk"
         width="100%"
         height="400"
-        frameborder="0"
-        allowfullscreen=""
+        frameBorder="0"
+        allowFullScreen=""
         title="Google Map: Apartment Waltraud, Haus Karlwirt, Ramsau am Dachstein"
       >
       </iframe>
